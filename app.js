468123e1921852d63a949ab403b7f0012882814a
davidtrikic <davidtrikic@outlook.com>
2021-07-05 17:46:16 +0200

    first version

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const gameRules = [
   [1,   0, 'x']
 ];
 
+const options = ['rock', 'paper', 'scissors'];
+const gameBtn = document.getElementById('play-button');
+
 // Generate computer's move
 const computerPlay = () => Math.floor(Math.random() * 3);
 
@@ -15,28 +18,27 @@ const playerMove = () => {
     input = prompt("Enter your move: ").toLowerCase();
   } while (input != "rock" && input != "paper" && input != "scissors");
 
-  switch (input) {
-    case "rock":
-      return 0;
-    case "paper":
-      return 1;
-    case "scissors":
-      return 2;
-    default:
-      break;
-    }
+  return options.indexOf(input);
 }
 
 
 
-const playRound = (player, computer) => {
-  let playerSelection = playerMove();
-  let computerSelection = computerPlay();
-  console.log("Computer: " + computerSelection);
-  console.log("Player: " + playerSelection);
 
-  if (player == computer) playRound();
-  else console.log(gameRules[computer][player]);
+const playRound = () => {
+  let playerSelection, computerSelection;
+  
+  playerSelection = playerMove();
+  computerSelection = computerPlay();
+
+  if (gameRules[computerSelection][playerSelection] != 'x') 
+  {
+    console.log(gameRules[computerSelection][playerSelection]);
+  }
+  else {
+    playerSelection = playerMove();
+    computerSelection = computerPlay();
+    playRound();
+  }
 };
 
-playRound(playerSelection, computerSelection);
+gameBtn.addEventListener('click', playRound);
