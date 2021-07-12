f263424cba4a1e4a3a8314ed7e21900c7ab41f9b
davidtrikic <davidtrikic@outlook.com>
2021-07-12 22:03:46 +0200

    add buttons and button logic

--- app.js
+++ app.js
@@ -6,70 +6,76 @@ const gameRules = [
 ];
 
 const options = ['Rock', 'Paper', 'Scissors'];
-const gameBtn = document.getElementById('play-button');
+const playerScoreDiv = document.querySelector('.player-score');
+const computerScoreDiv = document.querySelector('.computer-score');
+const result = document.querySelector('.result');
+let playerScore = 0, computerScore = 0;
 
 // Capitalize first letter to match options array, for a formatted output
 const capitalizeFirst = (string) => string.charAt(0).toUpperCase() + string.slice(1);
 
 // Player's move
-const getPlayerMove = () => {
-  let input = "";
-  do {
-    input = capitalizeFirst(prompt("Enter your move: ").toLowerCase());
-  } while (input != "Rock" && input != "Paper" && input != "Scissors");
-
-  return options.indexOf(input);
-}
+const getPlayerMove = (e) => e.target.id;
 
 // Generate computer's move
 const getComputerMove = () => Math.floor(Math.random() * 3);
 
+const keepScore = () => {
+  
+}
 // Play one round
-const playRound = () => {
+const playRound = (e) => {
   let playerSelection, computerSelection;
   
-  playerSelection = getPlayerMove();
+  playerSelection = getPlayerMove(e);
   computerSelection = getComputerMove();
 
   if (computerSelection != playerSelection) 
   {
     if (gameRules[computerSelection][playerSelection]) {
-      console.log(`You Won! ${options[playerSelection]} beats ${options[computerSelection]}`)
-      return 1;
+      console.log(`You Won! ${options[playerSelection]} beats ${options[computerSelection]}`);
+      playerScore++;
+      playerScoreDiv.textContent = +playerScore;
+      return;
     }
-    console.log(`You Lose! ${options[computerSelection]} beats ${options[playerSelection]}`)
-    return 0; 
+    console.log(`You Lose! ${options[computerSelection]} beats ${options[playerSelection]}`);
+    computerScore++;
+    computerScoreDiv.textContent = +computerScore;
+    return;
   }
   // If tie, repeat function call
   alert("It's a tie! Please repeat this round!")
-  return playRound();
+  return playRound(e);
 };
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', playRound));
 
 
 // Main game function
-const playGame = () => {
-  let playerScore = 0, computerScore = 0;
+// const playGame = () => {
+//   let playerScore = 0, computerScore = 0;
 
-  while (playerScore < 5 && computerScore < 5) {
-    if (playRound()) playerScore++
-    else computerScore++
-  }
-  console.log(`- Final result - \nYour score: ${playerScore} \nComputer score: ${computerScore}`);
+//   while (playerScore < 5 && computerScore < 5) {
+//     if (playRound()) playerScore++
+//     else computerScore++
+//   }
+//   console.log(`- Final result - \nYour score: ${playerScore} \nComputer score: ${computerScore}`);
 
-  if (playerScore === computerScore) {
-    console.log("It's a tie! No one won!");
-    return;
-  }
-  if (playerScore > computerScore) {
-    console.log("Congrats! You Won!");
-    return;
-  }
-  if (playerScore < computerScore) {
-    console.log("Sorry, You lost!");
-    return;
-  } 
-}
+//   if (playerScore === computerScore) {
+//     console.log("It's a tie! No one won!");
+//     return;
+//   }
+//   if (playerScore > computerScore) {
+//     console.log("Congrats! You Won!");
+//     return;
+//   }
+//   if (playerScore < computerScore) {
+//     console.log("Sorry, You lost!");
+//     return;
+//   } 
+// }
 
 // Start game
-gameBtn.addEventListener('click', playGame);
+// gameBtn.addEventListener('click', playRound);
 
