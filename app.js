3e945f62878704d810925514268643ce615cfcda
davidtrikic <davidtrikic@outlook.com>
2021-07-13 01:02:15 +0200

    add main ui elements, refactor code

--- app.js
+++ app.js
@@ -6,47 +6,53 @@ const gameRules = [
 ];
 
 const options = ['Rock', 'Paper', 'Scissors'];
-const playerScoreDiv = document.querySelector('.player-score');
-const computerScoreDiv = document.querySelector('.computer-score');
-const result = document.querySelector('.result');
-let playerScore = 0, computerScore = 0;
+const playerScoreDisplay = document.querySelector('.player-score p');
+const computerScoreDisplay = document.querySelector('.computer-score p');
+const gameStatusDisplay = document.querySelector('.game-status p');
+const scores = {
+  playerScore: 0,
+  computerScore: 0
+}
 
 // Capitalize first letter to match options array, for a formatted output
 const capitalizeFirst = (string) => string.charAt(0).toUpperCase() + string.slice(1);
 
-// Player's move
-const getPlayerMove = (e) => e.target.id;
-
 // Generate computer's move
 const getComputerMove = () => Math.floor(Math.random() * 3);
 
-const keepScore = () => {
-  
+const keepScore = (selection, computer, player) => {
+  if (selection) {
+    scores.playerScore++;
+    playerScoreDisplay.textContent = scores.playerScore;
+    gameStatusDisplay.textContent = `You Won! ${options[player]} beats ${options[computer]}`;
+    return;
+  }
+  scores.computerScore++;
+  computerScoreDisplay.textContent = scores.computerScore;
+  gameStatusDisplay.textContent = `You Lose! ${options[computer]} beats ${options[player]}`;
+  return;
 }
-// Play one round
+
+const hasWon = () => (scores.playerScore === 5 || scores.computerScore === 5);
+
+const gameEnd = () => {
+  buttons.forEach(button => button.disabled = true);
+}
 const playRound = (e) => {
   let playerSelection, computerSelection;
   
-  playerSelection = getPlayerMove(e);
+  playerSelection = e.target.id;
   computerSelection = getComputerMove();
 
   if (computerSelection != playerSelection) 
   {
-    if (gameRules[computerSelection][playerSelection]) {
-      console.log(`You Won! ${options[playerSelection]} beats ${options[computerSelection]}`);
-      playerScore++;
-      playerScoreDiv.textContent = +playerScore;
-      return;
-    }
-    console.log(`You Lose! ${options[computerSelection]} beats ${options[playerSelection]}`);
-    computerScore++;
-    computerScoreDiv.textContent = +computerScore;
+    keepScore(gameRules[computerSelection][playerSelection], computerSelection, playerSelection);
+    if (hasWon()) gameEnd();
     return;
   }
-  // If tie, repeat function call
-  alert("It's a tie! Please repeat this round!")
-  return playRound(e);
+  gameStatusDisplay.textContent = "It's a tie! Repeat this round!";
 };
+
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => button.addEventListener('click', playRound));
