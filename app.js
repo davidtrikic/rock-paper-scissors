8dc60a61202fa28cad77d1b8903bd1fb9245a461
davidtrikic <davidtrikic@outlook.com>
2021-07-13 19:09:12 +0200

    add main DOM functionality

--- app.js
+++ app.js
@@ -9,16 +9,26 @@ const options = ['Rock', 'Paper', 'Scissors'];
 const playerScoreDisplay = document.querySelector('.player-score p');
 const computerScoreDisplay = document.querySelector('.computer-score p');
 const gameStatusDisplay = document.querySelector('.game-status p');
+const playerIcon = document.querySelector('#player-icon');
+const computerIcon = document.querySelector('#computer-icon');
 const scores = {
   playerScore: 0,
   computerScore: 0
 }
-
-// Capitalize first letter to match options array, for a formatted output
-const capitalizeFirst = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+const iconClasses = {
+  question: "fa-question-circle",
+  Rock: "fa-hand-rock",
+  Paper: "fa-hand-paper",
+  Scissors: "fa-hand-peace"
+}
 
 // Generate computer's move
 const getComputerMove = () => Math.floor(Math.random() * 3);
+
+const toggleIcons = (e, computer) => {
+  computerIcon.setAttribute('class', `fas fa-8x ${iconClasses[options[computer]]}`);
+  playerIcon.setAttribute('class', `fa-8x ${e.target.className}` )
+}
 
 const keepScore = (selection, computer, player) => {
   if (selection) {
@@ -38,11 +48,12 @@ const hasWon = () => (scores.playerScore === 5 || scores.computerScore === 5);
 const gameEnd = () => {
   buttons.forEach(button => button.disabled = true);
 }
+
 const playRound = (e) => {
   let playerSelection, computerSelection;
-  
   playerSelection = e.target.id;
   computerSelection = getComputerMove();
+  toggleIcons(e, computerSelection);
 
   if (computerSelection != playerSelection) 
   {
@@ -53,35 +64,6 @@ const playRound = (e) => {
   gameStatusDisplay.textContent = "It's a tie! Repeat this round!";
 };
 
-
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => button.addEventListener('click', playRound));
-
-
-// Main game function
-// const playGame = () => {
-//   let playerScore = 0, computerScore = 0;
-
-//   while (playerScore < 5 && computerScore < 5) {
-//     if (playRound()) playerScore++
-//     else computerScore++
-//   }
-//   console.log(`- Final result - \nYour score: ${playerScore} \nComputer score: ${computerScore}`);
-
-//   if (playerScore === computerScore) {
-//     console.log("It's a tie! No one won!");
-//     return;
-//   }
-//   if (playerScore > computerScore) {
-//     console.log("Congrats! You Won!");
-//     return;
-//   }
-//   if (playerScore < computerScore) {
-//     console.log("Sorry, You lost!");
-//     return;
-//   } 
-// }
-
-// Start game
-// gameBtn.addEventListener('click', playRound);
 
